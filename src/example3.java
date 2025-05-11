9b8c736daf27bc4fc5d1a7e5c559e5195006b908
Nahida <2966066392@qq.com>
2025-05-12 01:09:15 +0800

    无

---
+++ src/example3.java
@@ -0,0 +1,11 @@
+package src;
+import GUI.Login;
+
+import javax.swing.*;
+
+public class example3 {
+    public static void main(String[] args) {
+            Login loginFrame = new Login();
+
+    }
+}
